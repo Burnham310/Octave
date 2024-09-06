23c8f98232fcc24e783917e93a8adfb0b03f1141
tesseract22 <caleb2222@qq.com>
2024-09-06 13:17:28 -0500

    lexer wip

---
+++ lexer.c
@@ -0,0 +1,63 @@
+
+#include "lexer.h"
+#include <assert.h>
+Lexer lexer_init(const char* src, size_t src_len, const char* path) {
+    Lexer lexer = {.src = src, .src_len = src_len, .off = 0, .path = path, .peekbuf = TK_NULL};
+
+    return lexer;
+}
+void skip_ws(Lexer *lexer) {
+    while (lexer->off < lexer->src_len) {
+	char c = lexer->src[lexer->off];
+	switch (c) {
+	    case '\n':
+	    case ' ':
+	    case '\t':
+		break;
+	    default:
+		return;
+	}
+	lexer->off++;
+    }
+}
+// return 0 if no more character left
+char next_char(Lexer* self) {
+    if (self->off >= self->src_len) return 0;
+    self->off++;
+    return self->src[self->off - 1];
+}
+#define RETURN_TK(x) do {tk.type = x; return tk; } while (0)
+Token match_single(Lexer *self) {
+
+    char c;
+    Token tk = {.off = self->off};
+    switch ((c = next_char(self))) {
+	case 0:
+	    RETURN_TK(TK_NULL);
+	case '=':
+	    RETURN_TK(TK_EQ);
+	case '[':
+	    RETURN_TK(TK_LBRAC);
+	case ']':
+	    RETURN_TK(TK_RRBRC);
+	case '.':
+	    RETURN_TK(TK_DOT);
+	case ',':
+	    RETURN_TK(TK_COMMA);
+	default:
+	    RETURN_TK(TK_NULL);
+	
+    }
+    return tk;
+
+}
+Token match_multiple(Lexer *self) {
+    
+}	
+Token match_num(Lexer *self) {
+
+}
+Token match_iden(Lexer *self) {
+    
+}
+void lexer_deinit(Lexer *lexer) {}
---
+++ lexer.h
@@ -0,0 +1,51 @@
+#ifndef LEXER_H_
+#define LEXER_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+typedef struct {
+    size_t	row;
+    size_t 	col;
+} Loc;
+typedef enum {
+    TK_IDEN,
+    TK_INT,
+
+    TK_EQ,
+    TK_COLON,
+    TK_LBRAC,
+    TK_RRBRC,
+    TK_DOT,
+    TK_COMMA,
+
+    TK_NULL,
+    TK_EOF,
+} TokenType;
+typedef union {
+    size_t 	integer;
+    char* 	str;
+} TokenData;
+typedef struct {
+    TokenType 	type;
+    TokenData 	data;
+    size_t	off;
+} Token;
+
+typedef struct {
+    const char* src;
+    size_t 	src_len;
+    size_t	off;
+    const char*	path;
+    Token 	peekbuf;
+} Lexer;
+Lexer lexer_init(const char* src, size_t src_len, const char* path);
+void skip_ws(Lexer *lexer); 
+// return 0 if no more character left
+char next_char(Lexer* self); 
+Token match_single(Lexer *self);
+Token match_multiple(Lexer *self);
+Token match_num(Lexer *self);
+Token match_iden(Lexer *self);
+
+void lexer_deinit(Lexer *lexer);
+#endif
