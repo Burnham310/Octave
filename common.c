47a328eb3a80f244754148451101824883cf381f
TwinIsland <twisland@outlook.com>
2024-09-24 17:52:56 -0500

    map scale

--- common.c
+++ common.c
@@ -8,9 +8,16 @@ enum MidiScaleType getMidiKeyType(const char *target, int len)
 {
     if (strncmp(target, "cmajor", len) == 0)
         return CMAJOR;
-
     if (strncmp(target, "cminor", len) == 0)
         return CMINOR;
+    if (strncmp(target, "dmajor", len) == 0)
+        return DMAJOR;
+    if (strncmp(target, "dminor", len) == 0)
+        return DMINOR;
+    if (strncmp(target, "emajor", len) == 0)
+        return EMAJOR;
+    if (strncmp(target, "eminor", len) == 0)
+        return EMINOR;
 
     return ERRSCALE;
 }
