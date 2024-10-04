1e0e311c7710c077326272245c7ff615831bd528
TwinIsland <twisland@outlook.com>
2024-10-04 03:55:00 -0500

    support pausing note event

--- entry/backend.c
+++ entry/backend.c
@@ -7,17 +7,18 @@ int main()
 
     unsigned char notes[] = {1, 1, 5, 5, 6, 6, 5}; // twinkle
 
-    init_midi_backend(fp, &(MidiConfig){.volume = 100});
+    init_midi_backend(fp, &(MidiConfig){.volume = 100, .devision = 120});
 
     add_midi_event(SetTempoEvent(120));
-    add_midi_event(SetInstrument(41));
-    for (int i = 0; i < sizeof(notes) / sizeof(notes[0]) * 100; ++i)
+    add_midi_event(SetInstrumentEvent(41));
+    for (int i = 0; i < sizeof(notes) / sizeof(notes[0]) * 10; ++i)
     {
 
         if (i == 10)
         {
             add_midi_event(SetTempoEvent(240));
-            add_midi_event(SetInstrument(55));
+            add_midi_event(SetInstrumentEvent(55));
+            add_midi_event(PauseNoteEvent(WHOLE_NOTE));
         }
 
         MidiNote cur_note = {
